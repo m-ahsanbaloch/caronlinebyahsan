faf2be9e19c2ffce13a72349b7dcac09a090ca02
ahsansamad <ingeniouslogo@gmail.com>
2024-06-23 17:44:11 +0500

    S

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
-var carName = document.getElementById("carName");
-var carModel = document.getElementById("carModel");
-var civic = document.getElementById("car");
-var details = document.getElementById("carDetails");
 var cars = {
   honda: {
     civic: {
       varient: "Civic Vti Oriel",
       model: "2023",
       price: 4000000,
-      image:{
-        black : "assets/images/logocar.png",
-        red : "assets/images/logocar.png",
-        white : "assets/images/logocar.png",
-
-      } 
+      image: {
+        black: "assets/images/logocar.png",
+        red: "assets/images/logocar.png",
+        white: "assets/images/logocar.png",
+      },
     },
     fit: {
       varient: "Fit Hybrid",
       model: "2020",
       price: 4000000,
-      image:{
-        black : "assets/images/logocar.png",
-        red : "assets/images/logocar.png",
-        white : "assets/images/logocar.png",
-
-      } 
+      image: {
+        black: "assets/images/logocar.png",
+        red: "assets/images/logocar.png",
+        white: "assets/images/logocar.png",
+      },
     },
     city: {
       varient: "City Aspire",
       model: "2022",
       price: 3500000,
-      image:{
-        black : "assets/images/logocar.png",
-        red : "assets/images/logocar.png",
-        white : "assets/images/logocar.png",
-
-      } 
+      image: {
+        black: "assets/images/logocar.png",
+        red: "assets/images/logocar.png",
+        white: "assets/images/logocar.png",
+      },
     },
   },
 
@@ -44,149 +37,90 @@ var cars = {
       varient: "altis Vvti ",
       model: "2023",
       price: 4000000,
-      image:{
-        black : "assets/images/logocar.png",
-        red : "assets/images/logocar.png",
-        white : "assets/images/logocar.png",
-
-      } 
-
+      image: {
+        black: "assets/images/logocar.png",
+        red: "assets/images/logocar.png",
+        white: "assets/images/logocar.png",
+      },
     },
     fortuner: {
       varient: "Sigma",
       model: "2022",
       price: 3500000,
-      image:{
-        black : "assets/images/logocar.png",
-        red : "assets/images/logocar.png",
-        white : "assets/images/logocar.png",
-
-      } 
-
+      image: {
+        black: "assets/images/logocar.png",
+        red: "assets/images/logocar.png",
+        white: "assets/images/logocar.png",
+      },
     },
   },
 };
+
+var carName = document.getElementById("carName");
+var carModel = document.getElementById("carModel");
+var civic = document.getElementById("car");
+var details = document.getElementById("carDetails");
+
 for (var key in cars) {
   carName.innerHTML += `<option  value="${key}"> ${key.toUpperCase()}</option>`;
 }
+
 function getVarient() {
+  details.innerHTML = ""
   var selectedManufacture = event.target.value;
-  carModel.innerHTML = `<option  value=""> Varient</option>`;
+  carModel.innerHTML = `<option  value="">Varient</option>`;
   if (selectedManufacture) {
     var models = cars[selectedManufacture];
   }
   for (var modelName in cars[selectedManufacture]) {
-    carModel.innerHTML += `<option  value=""> ${modelName.toUpperCase()}</option>`;
-    // console.log(modelName);
-  
-    // console.log(cars[selectedManufacture][modelName].model);
+    carModel.innerHTML += `<option  value="${modelName}"> ${modelName.toUpperCase()}</option>`;
+    details.innerHTML += `
+      <div class="card" style="width: 18rem;">
+                <img src="assets/images/civic.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${modelName}</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
 
-    // console.log("======>", cars[selectedManufacture][modelName]);
+    `;
   }
-
+  
 }
-
-
-
-function displayCarDetails(){
-for(var key in cars){
-
+function showDetails(event){
+  details.innerHTML  = ""
+  var selectedModel = event.target.value;
+  if(selectedModel){
+    var selectModel = cars[selectedModel.value]
+    // console.log(selectedModel)
+  }
+  for(var keyMy in cars){
+  // console.log(keyModel)
+  for(var keyModel in cars[keyMy]){
+    console.log(keyModel.civi)
+  }
 }
+  details.innerHTML += `
+  <div class="card" style="width: 33rem; ">
+            <img src="assets/images/civic.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${selectedModel}</h5>
+              <p class="card-text">Abcde</p>
+              <a href="#" class="btn btn-primary">Explore</a>
+            </div>
+          </div>
 
+`;
 
-
-
-
-
-
-// var carName = document.getElementById("carName");
-// var carModel = document.getElementById("carModel");
-// var carVarient = document.getElementById("carVarient");
-// var carPrice = document.getElementById("carPrice");
-// var carYear = document.getElementById("carYear");
-// var carImage = document.getElementById("carImage");
-
-// var cars = {
-//   honda: {
-//     civic: {
-//       varient: "Civic Vti Oriel",
-//       model: "2023",
-//       price: 4000000,
-//       image: {
-//         black: "assets/images/logocar.png",
-//         red: "assets/images/logocar.png",
-//         white: "assets/images/logocar.png",
-//       },
-//     },
-//     fit: {
-//       varient: "Fit Hybrid",
-//       model: "2020",
-//       price: 4000000,
-//       image: {
-//         black: "assets/images/logocar.png",
-//         red: "assets/images/logocar.png",
-//         white: "assets/images/logocar.png",
-//       },
-//     },
-//     city: {
-//       varient: "City Aspire",
-//       model: "2022",
-//       price: 3500000,
-//       image: {
-//         black: "assets/images/logocar.png",
-//         red: "assets/images/logocar.png",
-//         white: "assets/images/logocar.png",
-//       },
-//     },
-//   },
-
-//   toyota: {
-//     corolla: {
-//       varient: "altis Vvti ",
-//       model: "2023",
-//       price: 4000000,
-//       image: {
-//         black: "assets/images/logocar.png",
-//         red: "assets/images/logocar.png",
-//         white: "assets/images/logocar.png",
-//       },
-//     },
-//     fortuner: {
-//       varient: "Sigma",
-//       model: "2022",
-//       price: 3500000,
-//       image: {
-//         black: "assets/images/logocar.png",
-//         red: "assets/images/logocar.png",
-//         white: "assets/images/logocar.png",
-//       },
-//     },
-//   },
-// };
-
-// for (var key in cars) {
-//   carName.innerHTML += `<option value="${key}"> ${key.toUpperCase()}</option>`;
-// }
-
-// function getVarient() {
-//   var selectedManufacture = event.target.value;
-//   carModel.innerHTML = '<option value="">Select Model</option>';
-//   if (selectedManufacture) {
-//     var models = cars[selectedManufacture];
-//     for (var modelName in models) {
-//       carModel.innerHTML += `<option value="${modelName}">${modelName.toUpperCase()}</option>`;
-//     }
-//   }
-// }
-
-// function displayCarDetails() {
-//   var selectedManufacture = carName.value;
-//   var selectedModel = carModel.value;
-//   if (selectedManufacture && selectedModel) {
-//     var carDetails = cars[selectedManufacture][selectedModel];
-//     carVarient.innerText = `Varient: ${carDetails.varient}`;
-//     carPrice.innerText = `Price: ${carDetails.price}`;
-//     carYear.innerText = `Model Year: ${carDetails.model}`;
-//     carImage.src = carDetails.image.black; // Assuming you want to show the black image by default
-//   }
-// }
+  // for(var mDetails in selectModel){
+  //   console.log(mDetails)
+  // }
+  // for(var key in cars){
+  //   // console.log(cars[key])
+  //   for(var key2 in cars[key]){
+  //     // console.log(cars[key][key2])
+  //   }
+  // }
+}
+// console.log(modelName);
